a8399a246185ccb0cecf8fd2635bce7e378725b1
arthdarji352 <arthdarji20@gmail.com>
2024-01-19 21:41:15 +0530

    basic packages setup

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
 
+dotenv.config();
 //rest object
 const app = express();
 
 //middleware
+app.use(express.json());
+app.use(morgan("dev"));
+
 app.get("/test", (req, res) => {
-  res.status(200).send("<h1>Hello nodejs</h1>");
+  res.status(200).send("<h1>Hello nodejs with sql</h1>");
 });
 
 //port
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log("server is running");
+  console.log(`server is running on port ${PORT}`);
 });
